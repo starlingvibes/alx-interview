1739b474f5d9e7a7eb01aa039770dd0a95068282
starlingvibes <starlingroot@protonmail.com>
2023-03-26 19:33:28 +0100

    added docs to the module

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,6 +2,18 @@
 
 
 def makeChange(coins, total):
+    """
+    Given a pile of coins of different values, determine the fewest
+    number of coins needed to meet a given amount total.
+    Args:
+        coins: list of the values of the coins in your possession
+        total: total value to meet
+        Returns:
+            fewest number of coins needed to meet total
+            If total is 0 or less, return 0
+            If total cannot be met by any number of coins you have,
+            return -1
+            """
     if total <= 0:
         return 0
     coins.sort(reverse=True)
