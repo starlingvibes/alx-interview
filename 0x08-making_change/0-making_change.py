76e9b35b68c37a84cb127896decadc7a58687349
starlingvibes <starlingroot@protonmail.com>
2023-03-26 19:40:32 +0100

    made pycodestyle happy

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 """
-Given a pile of coins of different values, determine the fewest number of coins needed to meet a given amount total.
+Given a pile of coins of different values, determine the fewest number of coins
+needed to meet a given amount total.
 """
 
 
