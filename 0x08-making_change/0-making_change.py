55f420dd3208fa0d1674cb290410c00036d69f29
starlingvibes <starlingroot@protonmail.com>
2023-03-26 19:35:27 +0100

    added docs to the module

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
+"""
+Given a pile of coins of different values, determine the fewest number of coins needed to meet a given amount total.
+"""
+
 
 def makeChange(coins, total):
     """
