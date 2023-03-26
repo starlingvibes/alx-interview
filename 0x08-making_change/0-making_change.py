94b32ebbc6977a2f965b279783ce9fed80cf53a5
starlingvibes <starlingroot@protonmail.com>
2023-03-26 19:38:09 +0100

    added docs to the module

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -21,7 +21,6 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
     coins.sort(reverse=True)
-    print(coins)
     count = 0
     for coin in coins:
         while total >= coin:
