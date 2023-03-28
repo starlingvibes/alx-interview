90bfe62251ebf40fae146c80c511ec4871dde5f2
starlingvibes <starlingroot@protonmail.com>
2023-03-28 22:45:44 +0100

    chore: initial commit

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+Module to calculate the perimeter of an island
+"""
+
+
+def island_perimeter(grid):
+    """
+    Calculates and returns the perimeter of an island
+    """
+    pass
